5f0f63c62dedaef21fe6a5016cb942f320414235
Vora Pranjal <pgvora15@gmail.com>
2021-06-07 16:48:57 +0530

    Removed Unnecessary console.log call

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,6 @@
 
 
-console.log('Hi I am Javascript file');
+// console.log('Hi I am Javascript file');
 
 fetch('http://puzzle.mead.io/puzzle').then((response)=>{
     response.json().then((data)=>{
