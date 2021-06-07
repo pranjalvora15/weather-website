b9cde9b54ef5c417d4289eb7f97a6b3d4702f7b0
Vora Pranjal <pgvora15@gmail.com>
2021-06-07 22:27:33 +0530

    Add humidity error correction

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request=require('request')
 
 const forecast=(latitude,longitude,callback)=>{
     // const url='http://api.weatherstack.com/current?access_key=78312ab609faec03a11a425b5e08497c&query='+latitude+','+longitude+'&units=f'
-    const url='http://api.weatherstack.com/current?access_key=78312ab609faec03a11a425b5e08497c&query='+latitude+','+longitude+
+    const url='http://api.weatherstack.com/current?access_key=78312ab609faec03a11a425b5e08497c&query='+latitude+','+longitude
     request({url,json:true},(error,{body})=>{
       if(error){
         callback('unable to connect to weather service!',undefined)
