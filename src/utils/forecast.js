48b6ff2a226631b5f57af7483b7a5a8385a06488
Vora Pranjal <pgvora15@gmail.com>
2021-06-07 22:24:35 +0530

    Add humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,8 @@
 const request=require('request')
 
 const forecast=(latitude,longitude,callback)=>{
-    const url='http://api.weatherstack.com/current?access_key=78312ab609faec03a11a425b5e08497c&query='+latitude+','+longitude+'&units=f'
+    // const url='http://api.weatherstack.com/current?access_key=78312ab609faec03a11a425b5e08497c&query='+latitude+','+longitude+'&units=f'
+    const url='http://api.weatherstack.com/current?access_key=78312ab609faec03a11a425b5e08497c&query='+latitude+','+longitude+
     request({url,json:true},(error,{body})=>{
       if(error){
         callback('unable to connect to weather service!',undefined)
@@ -12,9 +13,9 @@ const forecast=(latitude,longitude,callback)=>{
           body.current.weather_descriptions[0] +
       '. It is currently ' +
       body.current.temperature +
-      ' Farrenheit out. It feels like ' +
+      ' degrees out. It feels like ' +
       body.current.feelslike +
-      ' Farrenheit.'
+      ' degrees.\n'+'Humidity is '+body.current.humidity+'%.'
           // weather_description:response.body.current.weather_descriptions[0],
           // current_temperature:response.body.current.temperature,
           // feelslike:response.body.current.feelslike
